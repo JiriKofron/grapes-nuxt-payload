6ee5bddab2d9da0e2abe984090c3f5525d1e608a
Jiří Kofroň <kofron.jiri@gmail.com>
2024-06-07 20:02:33 +0200

    add grapes and create pages

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    hu
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import 'grapesjs/dist/css/grapes.min.css';
+import grapesjs from 'grapesjs';
+
+onMounted(() => {
+  const editor = grapesjs.init({
+    container: '#gjs',
+    height: 600,
+    width: '100%',
+    // ... other options
+  });
+})
+</script>
+
+<template>
+<div>
+  <h1 class="text-red-400">index page</h1>
+  <div id="gjs">
+    What is this?
+  </div>
+</div>
+</template>
